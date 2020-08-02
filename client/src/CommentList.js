d7f3432f7bf2326aa65ea58307cf70c4f58070dc
fevi <f.viscomi@redev.technology>
2020-08-02 18:02:12 +0100

    022

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -1,23 +1,24 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-export default ({postId}) => {
-    const [comments, setComments] = useState([]);
-    const fetchData = async () => {
-        const res = await axios.get(`http://localhost:4001/posts/${postId}/comments`);
-        setComments(res.data);
-    };
-    useEffect(
-        () => {
-            fetchData();
-        },
-        []
+export default ({ postId }) => {
+  const [comments, setComments] = useState([]);
+
+  const fetchData = async () => {
+    const res = await axios.get(
+      `http://localhost:4001/posts/${postId}/comments`
     );
-    const renderedComments = comments.map(comment => {
-        console.log(comment);
-        return <li key={comment.id}>{comment.content}</li>;
-    });
-    return <ul>
-        {renderedComments}
-    </ul>
+
+    setComments(res.data);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const renderedComments = comments.map(comment => {
+    return <li key={comment.id}>{comment.content}</li>;
+  });
+
+  return <ul>{renderedComments}</ul>;
 };
